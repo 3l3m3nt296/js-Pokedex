ebf94d55f90e4be6623870009cd0f989a03a4a75
3l3m3nt296 <83665044+3l3m3nt296@users.noreply.github.com>
2021-06-27 15:19:35 +0200

    update alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
- let myname ='hamburguer  ';
- document.write(myname);
- myname ='hot dog  ';
- document.write(myname);
- myname = 'sushi  ';
- document.write(myname);
- myname ='salad  ';
- document.write(myname);
- myname = 'soup';
- document.write(myname);
+alert('Hello world')
+
+let Favoritefood='hamburguer  ';
+ document.write(Favoritefood);
+ Favoritefood='hot dog  ';
+ document.write(Favoritefood);
+ Favoritefood = 'sushi  ';
+ document.write(Favoritefood);
+ Favoritefood ='salad  ';
+ document.write(Favoritefood);
+ Favoritefood = 'soup';
+ document.write(Favoritefood);
