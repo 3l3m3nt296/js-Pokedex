013e035baad947dac3efcd54535882bd2e71ecc8
3l3m3nt296 <83665044+3l3m3nt296@users.noreply.github.com>
2021-06-28 19:37:13 +0200

    update js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,36 @@
-alert('Hello world')
+let pokemonList = [
+    {
+      name: 'Bulbasur',
+      height: 0.7,
+      types: ['plant','poison'],
+      Number: 1,
+    },
+    {
+      name: 'Bisaknosp',
+      height: 1.0,
+      types: ['plant','poison'],
+      Number: 2,
+    },
+    {
+      name: 'MBisaflor',
+      height: 2,
+      types: ['plant','poison'],
+      Number: 3,
+    },
+  ];
+// unordered list format
+document.write('<ul>');
 
-let Favoritefood='hamburguer  ';
- document.write(Favoritefood);
- Favoritefood='hot dog  ';
- document.write(Favoritefood);
- Favoritefood = 'sushi  ';
- document.write(Favoritefood);
- Favoritefood ='salad  ';
- document.write(Favoritefood);
- Favoritefood = 'soup';
- document.write(Favoritefood);
+// List each Pokemon on the html page
+for (let i = 0; i < pokemonList.length; i++){
+  document.write('<li>');
+    // Include name and height
+    document.write(pokemonList[i].name + ' (height ' + pokemonList[i].height + ')');
+    // If height is over 1m, write "Wow, That is big!"
+    if (pokemonList[i].height > 1) document.write(' - cool, that\'s nice! ');
+  document.write('</li>');
+}
+
+document.write('</ul>');
+
+  
