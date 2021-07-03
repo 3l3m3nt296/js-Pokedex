4227cf069023e74cdb1aa72ea9ffd5ca2863e8e0
3l3m3nt296 <83665044+3l3m3nt296@users.noreply.github.com>
2021-07-03 15:18:58 +0200

    scripts data types

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,44 @@
-let pokemonRepository = (function()  {
+
 let pokemonList = [
     {
       name: 'Bulbasur',
-      types: ['plant','poison'],
       height: 0.7,
+      types: ['plant','poison'],
+      number: 1,
+     
     },
     {
       name: 'Bisaknosp',
-      types: ['plant','poison'],
       height: 1.0,
+      types: ['plant','poison'],
+      number: 3,
+  
+     
     },
     {
       name: 'MBisaflor',
-      types: ['plant','poison'],
       height: 2,
+      types: ['plant','poison'],
+      number:4
+     
     }
   ];
 
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-  function getAll() {
-    return pokemonList;
-  }
-  return {
-    add: add,
-    getAll: getAll
-  };
-})();
 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Ren' });
-console.log(pokemonRepository.getAll());
+  // unordered list format
+document.write('<ul>');
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write(pokemon.name + " " + "height : " + pokemon.height + "" + pokemon.type + "</br>");
-});
-
-/* for (let i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + "- Height: " + pokemonList[i].height)
-  console.log(pokemonList[i].name + " Height: " + pokemonList[i].height)
-if ([pokemonList[i].height] < 0.5) {
-  document.write(" - This Pokemon is SMOL!")
+// List each Pokemon on the html page
+for (let i = 0; i < pokemonList.length; i++){
+  document.write('<li>');
+    // Include name and height
+    document.write(pokemonList[i].name + ' (height ' + pokemonList[i].height + ')');
+    // If height is over 1m, write "Wow, That is big!"
+    if (pokemonList[i].height > 1) document.write(' - Wow, that\'s big! ');
+  document.write('</li>');
 }
- document.write("<br>");
-} */
 
-/*pokemonList.forEach(function(pokemon){
-  console.log(pokemon);
-});*/
+document.write('</ul>');
+
+
+
